cea863e161f3239daa0bca050f9cdb4075108ee0
prashant parajuli <prashanta.parajuli@macpokhara.edu.np>
2022-06-04 20:23:28 +0545

    fixed registration error

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
-const { User } = require('../models/user');
+const { Users } = require('../models/user');
 
 router.get('/', async(req, res) => {
     res.send('hello')
@@ -15,12 +15,12 @@ router.post('/register', async(req, res) => {
         password: await bcrypt.hashSync(req.body.password, 12),
     }
     try {
-        User.find({ email }, async(error, users) => {
+        Users.find({ email: req.body.email }, async(error, users) => {
             if (error) return res.send({ status: 'error', error: error.message });
             if (users.length > 0) {
                 return res.send({ status: 'error', error: 'user already exixsts' });
             } else {
-                const user = new User(userdata);
+                const user = new Users(userData);
                 const result = user.save();
                 return res.send({ status: 'success', data: { user: user } });
             }
